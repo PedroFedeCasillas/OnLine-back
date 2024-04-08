266dc706f0f19b862e7fb655f56d605aca70bf08
Pedro Fede Casillas <pedrofedecasillas2@gmail.com>
2024-04-08 17:45:50 -0300

    first commit

---
+++ src/main.ts
@@ -0,0 +1,9 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  await app.listen(3001);
+  console.log('Listening in the port', 3001)
+}
+bootstrap();
