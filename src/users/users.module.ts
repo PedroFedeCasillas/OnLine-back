26101048cb36ddea62dd0d0cd943dfaa3e91ae16
Pedro Fede Casillas <pedrofedecasillas2@gmail.com>
2024-04-09 09:26:44 -0300

    config add .env + module users, package.json

---
+++ src/users/users.module.ts
@@ -0,0 +1,9 @@
+import { Module } from '@nestjs/common';
+import { UsersService } from './services/users.service';
+import { UsersController } from './controllers/users.controller';
+
+@Module({
+  controllers: [UsersController],
+  providers: [UsersService]
+})
+export class UsersModule {}
