839b76fc0c94154a4ff7919493ff250c5d768c47
Pedro Fede Casillas <pedrofedecasillas2@gmail.com>
2024-04-26 15:32:45 -0300

    fix errors handlers

--- src/users/db/mongodb.service.ts
+++ src/users/db/mongodb.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model, mongo } from 'mongoose';
 import { CreateUserDto } from '../dto/create-user.dto';
@@ -6,6 +6,7 @@ import { User } from '../entities/user.entity';
 import { UpdateUserDto } from '../dto/update-user.dto';
 import { IUserDao } from './userDao';
 import { mongoExceptionHandler } from 'src/common/mongoExceptionHandler';
+import { ErrorManager } from 'src/utils/error.manager';
 
 @Injectable()
 export class MongoDbService implements IUserDao {
@@ -15,13 +16,31 @@ export class MongoDbService implements IUserDao {
 
   async create(createUserDto: CreateUserDto): Promise<User> {
     try {
+      const existingUser = await this._userModel.findOne(createUserDto).exec();
+      if (existingUser) {
+        throw new ErrorManager({
+          type: 'BAD_REQUEST',
+          message: 'El usuario ya existe.',
+        });
+      }
+
       const createdUser = new this._userModel(createUserDto);
-      return createdUser.save();
+      return await createdUser.save();
     } catch (error) {
       if (error instanceof mongo.MongoError) mongoExceptionHandler(error);
       else throw error;
     }
   }
+
+  // async create(createUserDto: CreateUserDto): Promise<User> {
+  //   try {
+  //     const createdUser = new this._userModel(createUserDto);
+  //     return createdUser.save();
+  //   } catch (error) {
+  //     if (error instanceof mongo.MongoError) mongoExceptionHandler(error);
+  //     else throw error;
+  //   }
+  // }
 
   async findAll(): Promise<User[]> {
     try {
@@ -38,10 +57,12 @@ export class MongoDbService implements IUserDao {
 
   async findOne(id: string): Promise<User> {
     try {
-      const user = await this._userModel.findById(id)
-        .exec();
+      const user = await this._userModel.findById(id).exec();
       if (!user) {
-          throw new NotFoundException('User not found');
+        throw new ErrorManager({
+          type: 'BAD_REQUEST',
+          message: `No user found with id ${id}`,
+        });
       }
       return user;
     } catch (error) {
@@ -56,7 +77,10 @@ export class MongoDbService implements IUserDao {
         .findByIdAndUpdate(id, updateUserDto, { new: true })
         .exec();
       if (!updatedUser) {
-        throw new NotFoundException('User not found');
+        throw new ErrorManager({
+          type: 'BAD_REQUEST',
+          message: `Error updating with id ${id}`,
+        });
       }
       return updatedUser;
     } catch (error) {
@@ -69,7 +93,10 @@ export class MongoDbService implements IUserDao {
     try {
       const deletedUser = await this._userModel.findByIdAndDelete(id).exec();
       if (!deletedUser) {
-        throw new NotFoundException('User not found');
+        throw new ErrorManager({
+          type: 'BAD_REQUEST',
+          message: `Error when deleting with id ${id}`,
+        });
       }
       return deletedUser;
     } catch (error) {
